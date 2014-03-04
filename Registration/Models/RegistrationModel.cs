dad3a7dec11743e1d13a5149e7f3fd15c1df2bcc
Ahsan <vampxlr@yahoo.com>
2014-03-05 03:58:10 +0600

    auto increment

--- Registration/Models/RegistrationModel.cs
+++ Registration/Models/RegistrationModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace Registration.Models
     public class RegistrationModel
     {
         [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int id { get; set; }
         [Required]
         public string FullName { get; set; }
